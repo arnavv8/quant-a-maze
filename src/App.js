8eddf658058fb3d480453969993391e400a0ecf0
arnavv8 <arnav8131@gmail.com>
2024-03-28 20:10:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import NavbarComponent from './components/Navbar';
 import Header from './components/Header';
 import Hero from './components/Hero';
