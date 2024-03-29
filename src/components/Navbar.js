b375d1164b80349f9e097870e8a147df8042f052
arnavv8 <arnav8131@gmail.com>
2024-03-29 19:50:15 +0530

    c

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,13 +12,15 @@ const NavbarComponent = () => {
 
   return (
     <nav className="navbar">
-      <button onClick={() => scrollToSection('hero')}>Hero</button>
-      <button onClick={() => scrollToSection('tracks')}>Tracks</button>
-      <button onClick={() => scrollToSection('prizes')}>Prizes</button>
-      <button onClick={() => scrollToSection('sponsors')}>Sponsors</button>
-      <button onClick={() => scrollToSection('timeline')}>Timeline</button>
-      <button onClick={() => scrollToSection('contact')}>Contact</button>
-      {/* Add more navigation buttons as needed */}
+      <div className="navbar-items">
+        <button onClick={() => scrollToSection('hero')}>Hero</button>
+        <button onClick={() => scrollToSection('tracks')}>Tracks</button>
+        <button onClick={() => scrollToSection('prizes')}>Prizes</button>
+        {/* Add more navigation buttons as needed */}
+        <button onClick={() => scrollToSection('sponsors')}>Sponsors</button>
+        <button onClick={() => scrollToSection('faq')}>FAQ</button>
+        <button onClick={() => scrollToSection('contact')}>Contact</button>
+      </div>
     </nav>
   );
 };
