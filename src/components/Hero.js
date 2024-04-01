db08462ecc2a36de0e5922cb0a2bad931587b0f6
arnavv8 <arnav8131@gmail.com>
2024-04-01 23:25:12 +0530

    c

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+// Hero.js
 import React from 'react';
 import './Hero.css';
 
@@ -5,12 +6,18 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="hero-heading">
-        <h1>Quant-A-Maze</h1>
-        <h2>24-hour Hackathon</h2>
+        <h1>
+          QUANT<span className="yellow">-</span>A<span className="yellow">-</span>MAZE
+        </h1>
+        <h2>48-Hour Hackathon</h2>
       </div>
-      <button className="register-button" onClick={registerNow}>Register Now</button>
-      <p>June 2024</p>
-      <p>Nitte Meenakshi Institute of Technology, Bangalore</p>
+      <button className="register-button yellow-button" onClick={registerNow}>
+        Register Now
+      </button>
+      <div className="hero-subheading">
+        <p>24th - 26th April</p>
+        <p>Nitte Meenakshi Institute of Technology, Bangalore</p>
+      </div>
     </section>
   );
 };
