3a6eabe4784e043bede6f5ad9bc0a04a215fd67b
arnavv8 <arnav8131@gmail.com>
2024-04-10 21:45:08 +0530

    c

--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -12,13 +12,13 @@ const Prizes = () => {
         <img src={prizesbg} alt="Prizes Header" />
       </div>
       <div className="prizes-list">
-        <div className="prize-item">
+        <div className="prize-item prize-third">
           <img src={thirdprize} alt="3rd Prize" />
         </div>
-        <div className="prize-item">
+        <div className="prize-item prize-second">
           <img src={secondprize} alt="2nd Prize" />
         </div>
-        <div className="prize-item">
+        <div className="prize-item prize-first">
           <img src={firstprize} alt="1st Prize" />
         </div>
       </div>
