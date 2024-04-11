41e4161bc07bb3cfbaf654af5f6678519c308d5e
arnavv8 <arnav8131@gmail.com>
2024-04-11 17:37:58 +0530

    c

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Timeline.css';
-import timelineimage from '../img/timeline/timeline.png';
+import timelineImage from '../img/timeline/timeline.png';
+import timelineImage1 from '../img/timeline/timeline1.png';
 
-const Timeline = ({ imagePath }) => {
+const Timeline = (_) => {
   return (
     <section className="timeline-container" id="timeline">
       <div className="timeline-image">
-        <img src={timelineimage} alt="Timeline" />
+        <div className="timeline-image-1">
+          <img src={timelineImage} alt="Timeline" />
+        </div>
+        <div className="timeline-image-2">
+          <img src={timelineImage1} alt="Timeline1" />
+        </div>
       </div>
       <div className="timeline-content">
         {/* Add your timeline content here */}
